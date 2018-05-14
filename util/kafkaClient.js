7f362dea66589ca1cd7ddde9da17a72ebf921e1d
akane34 <jarvein@hotmail.com>
2018-05-14 12:53:21 -0500

    Fix problem with kafka connection

---
+++ util/kafkaClient.js
@@ -0,0 +1,73 @@
+var kafka = require('kafka-node');
+
+var kafkaConsumer;
+var kafkaProducer;
+
+const TOPIC = "lumenconcept.search";
+const KAFKA_HOST = process.env.KAFKA_SERVER + ':' + process.env.KAFKA_PORT;
+
+// public methods
+
+exports.suscribe = suscribe;
+exports.notify = notify;
+
+// private methods
+
+function suscribe(callback) {
+    var consumer = getConsumer();
+
+    consumer.on('message', function (message) {
+        if(callback) callback(message);
+    });
+
+    consumer.on('error', function (err) {
+        console.log('Error:',err);
+    });
+
+    consumer.on('offsetOutOfRange', function (err) {
+        console.log('OffsetOutOfRange:',err);
+    });
+}
+
+function notify(params, callback) {
+    var producer = getProducer(),
+        payloads = [
+            { topic: TOPIC, messages: params, partition: 0 }
+        ];
+
+    producer.on('ready', function () {
+        console.log('Producer is ready');
+    });
+
+    producer.on('error', function (err) {
+        console.log('Producer is in error state');
+        console.log(err);
+    });
+
+    producer.send(payloads, function (err, data) {
+        console.log('Connecting to... ', KAFKA_HOST, ' topic:', TOPIC);
+        if(callback) callback(err || data);
+    });
+}
+
+function getConsumer() {
+    if (!kafkaConsumer) {
+        var Consumer = kafka.Consumer,
+            client = new kafka.KafkaClient({kafkaHost: KAFKA_HOST}),
+            payloads = [{ topic: TOPIC, offset: 0}],
+            options = {autoCommit: false};
+        kafkaConsumer = new Consumer(client, payloads, options);
+    }
+
+    return kafkaConsumer;
+}
+
+function getProducer() {
+    if (!kafkaProducer) {
+        var Producer = kafka.Producer,
+            client = new kafka.KafkaClient({kafkaHost: KAFKA_HOST});
+        kafkaProducer = new Producer(client);
+    }
+
+    return kafkaProducer;
+}
--- util/tools.js
+++ util/tools.js
@@ -1,13 +1,4 @@
-const kafkaServer = process.env.KAFKA_SERVER || 'localhost';
-const kafkaPort = process.env.KAFKA_PORT || '9092';
-const   kafka = require('kafka-node'),
-    Producer = kafka.Producer,
-    client = new kafka.Client(kafkaServer + ':' + kafkaPort),
-    queueProducer = new Producer(client, {
-        requireAcks: 1,
-        ackTimeoutMs: 100,
-        partitionerType: 2
-    });
+var kafka = require('./kafkaClient');
 
 function sendResponse(data, isSuccess, res){
 
@@ -23,17 +14,10 @@ function sendResponse(data, isSuccess, res){
 }
 
 function sendMessageToQueue(search){
-    var payloads = [
-        { topic: 'lumenconcept.search', messages: search, partition: 0 }
-    ];
-
-    queueProducer.send(payloads, function (err, data) {
-        console.log('QUEUE SEND MESSAGE: ' + (err || data));
+    kafka.notify(search, function (data){
+        console.log('QUEUE SEND MESSAGE: ', data);
     });
 }
-
-queueProducer.on('ready', function () {console.log('queueProducer ready');});
-queueProducer.on('error', function (err) {console.log('ERROR QUEUE SEND MESSAGE: ' + err);})
 
 exports.sendResponse = sendResponse;
 exports.sendMessageToQueue = sendMessageToQueue;
